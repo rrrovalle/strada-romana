4f693c10048548ae4614013435ebbd3f3f620c5a
Rodrigo Valle <digo.valle09@yahoo.com.br>
2020-07-29 12:50:12 -0300

    Implementa padrão factory

--- Strada Romana/src/br/udesc/ppr55/sr/model/abstractFactory/AbstractPieceFactory.java
+++ Strada Romana/src/br/udesc/ppr55/sr/model/abstractFactory/AbstractPieceFactory.java
@@ -1,5 +1,7 @@
 package br.udesc.ppr55.sr.model.abstractFactory;
 
+import br.udesc.ppr55.sr.model.Piece;
+
 /**
  * Abstract Factory for Piece creation
  * @author Rodrigo Valle
@@ -7,5 +9,68 @@ package br.udesc.ppr55.sr.model.abstractFactory;
  * @version 1.0
  */
 public abstract class AbstractPieceFactory {
-    
+	
+	public abstract Piece createBag();
+	
+	public abstract Piece createCoin();
+	
+	public abstract Piece createVictoryPoint();
+	
+	public abstract Piece createBlueCube();
+	
+	public abstract Piece createBlueWareTile();
+	
+	public abstract Piece createBlueContract();
+	
+	public abstract Piece createBrownCube();
+	
+	public abstract Piece createBrownWareTile();
+	
+	public abstract Piece createBrownContract();
+	
+	public abstract Piece createGreenCube();
+	
+	public abstract Piece createGreenWareTile();
+	
+	public abstract Piece createGreenContract();
+	
+	public abstract Piece createRedCube();
+	
+	public abstract Piece createRedWareTile();
+	
+	public abstract Piece createRedContract();
+	
+	public abstract Piece createWhiteCube();
+	
+	public abstract Piece createWhiteWareTile();
+	
+	public abstract Piece createWhiteContract();
+	
+	public abstract Piece createYellowCube();
+	
+	public abstract Piece createYellowWareTile();
+	
+	public abstract Piece createYellowContract();
+	
+	public abstract Piece createBallioWagon(int side);
+	
+	public abstract Piece createCanopitesWagon(int side);
+	
+	public abstract Piece createCurculioWagon(int side);
+
+	public abstract Piece createDemetriusWagon(int side);
+	
+	public abstract Piece createHamilcarWagon(int side);
+	
+	public abstract Piece createHerenniusWagon(int side);
+	
+	public abstract Piece createMaccusWagon(int side);
+	
+	public abstract Piece createPersaWagon(int side);
+	
+	public abstract Piece createPlotusWagon(int side);
+	
+	public abstract Piece createPseudolousWagon(int side);
+	
+	//implementar deck para apostas
 }
--- Strada Romana/src/br/udesc/ppr55/sr/model/abstractFactory/PieceFactory.java
+++ Strada Romana/src/br/udesc/ppr55/sr/model/abstractFactory/PieceFactory.java
@@ -1,11 +1,199 @@
 package br.udesc.ppr55.sr.model.abstractFactory;
 
+import br.udesc.ppr55.sr.model.Piece;
+
 /**
  * Piece Factory class
  * @author Rodrigo Valle
  * @since 15/07/2020
  * @version 1.0
  */
-public class PieceFactory {
+public class PieceFactory extends AbstractPieceFactory{
+
+	@Override
+	public Piece createBag() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createCoin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createVictoryPoint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createBlueCube() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createBlueWareTile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createBlueContract() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createBrownCube() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createBrownWareTile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createBrownContract() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createGreenCube() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createGreenWareTile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createGreenContract() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createRedCube() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createRedWareTile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createRedContract() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createWhiteCube() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createWhiteWareTile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createWhiteContract() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createYellowCube() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createYellowWareTile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createYellowContract() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createBallioWagon(int side) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createCanopitesWagon(int side) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createCurculioWagon(int side) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createDemetriusWagon(int side) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createHamilcarWagon(int side) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createHerenniusWagon(int side) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createMaccusWagon(int side) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createPersaWagon(int side) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createPlotusWagon(int side) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece createPseudolousWagon(int side) {
+		// TODO Auto-generated method stub
+		return null;
+	}
     
 }
